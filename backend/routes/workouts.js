2a1e263d6cfefaf8c2e5fa8d68798a6dc0c35068
Abrar <abrarmurad00@gmail.com>
2023-11-25 15:24:20 -0500

    added REST operations

---
+++ backend/routes/workouts.js
@@ -0,0 +1,32 @@
+const express = require("express")
+
+const router = express.Router()
+
+// get all workouts
+router.get('/', (req, res) => {
+    res.json({mssg: 'GET ALL WORKOUTS'})
+})
+
+// get a single workout
+router.get('/:id', (req, res) => {
+    res.json({mssg: 'GET SINGLE WORKOUT'})
+})
+
+// post new workout
+router.post('/', (req, res) => {
+    res.json({mssg: 'POST A WORKOUT'})
+})
+
+// delete a workout
+router.delete('/:id', (req, res) => {
+    res.json({mssg: 'DELETE A WORKOUT'})
+})
+
+// update a workout
+router.patch('/:id', (req, res) => {
+    res.json({mssg: 'UPDATE A WORKOUT'})
+})
+
+
+
+module.exports = router
